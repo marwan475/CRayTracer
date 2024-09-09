df762410f152f636bd872af576df9775a6cf1ef5
Marwan <marwanmo475@gmail.com>
2024-09-09 15:42:34 -0600

    cleaned up intial renderer and added comments

--- renderer.c
+++ renderer.c
@@ -8,29 +8,25 @@ int main(){
   int j;
 
   // rgb values;
-  double r;
-  double g;
-  double b;
-  int ri;
-  int gi;
-  int bi;  
+  int r;
+  int g;
+  int b;
 
   int width = 256;
   int height = 256;
 
+  // ppm file header
   printf("P3\n%d %d\n255\n",width,height);
 
+  // formating rgb
   for ( j = 0; j < height;j++){
     for (i = 0; i < width;i++){
-      r = double(i) / (width - 1);
-      g = double(j) / (height -1);
-      b = 0.0;
+      r = i;
+      g = j;
+      b = (i + j)/2;
 
-      ri = int(255.99 * r);
-      gi = int(255.99 * g);
-      bi = int(255.99 * b);
-
-      printf("%d %d %d\n",ri,gi,bi);
+      //rgb values of each pixel in ppm format
+      printf("%d %d %d\n",r,g,b);
     } 
   }
 
