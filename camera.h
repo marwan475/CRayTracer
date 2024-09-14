c3c4536171b9161f7045605bedd215b78ac3d10c
Marwan <marwanmo475@gmail.com>
2024-09-14 01:21:31 -0600

    moved camera code into camera class

---
+++ camera.h
@@ -0,0 +1,100 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <limits>
+#include "objects.h"
+
+class camera {
+  public:
+
+    camera(double ar, int w,double vh,double fl,vec3 cc)
+    {
+      width = w;
+      aspect_ratio = ar;
+      
+      height = int(width/aspect_ratio);
+
+      viewport_h = vh;
+      viewport_w = viewport_h*(double(width)/double(height));
+
+      focal_length = fl;
+      ccenter = cc;
+ 
+      viewport_u = vec3(viewport_w,0,0); // horizontal vector of viewport
+      viewport_v = vec3(0,-viewport_h,0); // negative vertical vector of viewport
+
+      delta_u = SdivideVector(width,viewport_u); // change between pixels horizotanlly
+      delta_v = SdivideVector(height,viewport_v);// change between pixels vertically
+
+      vp_upperleft = subtractVectors(subtractVectors(subtractVectors(ccenter,vec3(0,0,focal_length)),SdivideVector(2,viewport_u)),SdivideVector(2,viewport_v));  
+
+      pixel_location = addVectors(vp_upperleft,SmultiVector(0.5,addVectors(delta_u,delta_v)));
+
+    }
+
+
+    void render(obj_list scene)
+    {
+      int i;
+      int j;      
+      vec3 center;
+      vec3 ray_direction;
+
+      printf("P3\n%d %d\n255\n",width,height);
+      
+      for ( j = 0; j < height;j++){
+        // progress tracker
+        fprintf(stderr,"\rRendering %f percent complete",(double(j)/double(height))*100);
+        fflush(stderr);
+        for (i = 0; i < width;i++){
+
+          // curent pixel
+          center = addVectors(pixel_location,addVectors(SmultiVector(i,delta_u),SmultiVector(j,delta_v)));
+          // ray diriciton to the current pixel
+          ray_direction = subtractVectors(center,ccenter);
+
+          // current ray
+          ray r = ray(ccenter,ray_direction);
+
+          // color from current ray
+          c = ray_color(r,scene);
+
+          //rgb values of each pixel in ppm format
+          printVector(stdout,SmultiVector(255.99,c));
+       }
+     }
+     fprintf(stderr,"\nComplete");
+    }
+
+    vec3 ray_color(ray r,obj_list scene)
+    {
+      obj_record rec;
+      if (scene.contact(r,interval(0,numeric_limits<double>::infinity()),&rec)){
+        return SmultiVector(0.5,addVectors(rec.normalV,vec3(1,1,1)));
+      }
+  
+
+      // Ray hit background	
+      vec3 ud = unitVector(r.dir());
+      double n = 0.7*(ud.y() + 1.0);
+      return addVectors(SmultiVector(1.0-n,vec3(1.0,1.0,1.0)),SmultiVector(n,vec3(1.0,0.7,0.5)));
+    }
+
+  private:
+    double aspect_ratio;
+    int width;
+    int height;
+    double viewport_h;
+    double viewport_w;
+    double focal_length;
+    vec3 ccenter;
+    vec3 viewport_u;
+    vec3 viewport_v;
+    vec3 delta_u;
+    vec3 delta_v;
+    vec3 vp_upperleft;
+    vec3 pixel_location;
+
+};
+
+#endif
